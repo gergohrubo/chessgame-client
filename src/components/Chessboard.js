20613d4837ef28d4c80a1fa30502ebf2c704d231
gergohrubo <gergohrubo@gmail.com>
2019-12-18 18:25:51 +0100

    Moving pieces around the board done

--- src/components/Chessboard.js
+++ src/components/Chessboard.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import '../styles/Chessboard.css'
 
+
 function Chessboard(props) {
-  const boardBackground = Array(4).fill([[0, 1, 0, 1, 0, 1, 0, 1], [1, 0, 1, 0, 1, 0, 1, 0]]).flat()
-  console.log(boardBackground)
   return (
     <div className="chessboard">
-      <p>Chessboard comes here</p>
-      {boardBackground.map((row, indexRow) => {
+      <br />
+      {props.board.map((row, indexRow) => {
         return <div className="chessboard-row">{row.map((square, indexColumn) => (
-          square ?
-            <div className="square-black"></div> : <div className="square-white"></div>))}</div>
+          ((indexRow + indexColumn) % 2 === 1) ?
+            <div key={indexRow * 8 + indexColumn} className="square-black" onClick={() => props.onClick(square)}><img key={indexRow * 8 + indexColumn + 100} className="piece-img" src={square.imgsrc} /></div> : <div key={indexRow * 8 + indexColumn} className="square-white" onClick={() => props.onClick(square)}><img key={indexRow * 8 + indexColumn + 100} className="piece-img" src={square.imgsrc} /></div>))}</div>
       })}
     </div>
   );
--- src/components/ChessboardContainer.js
+++ src/components/ChessboardContainer.js
@@ -1,24 +1,127 @@
 import React, { Component } from 'react';
 import Chessboard from './Chessboard'
+import Pieces from './Pieces'
+import { connect } from 'react-redux'
+import imgSrcAssigner from '../imgSrcAssigner'
+import { makeMove } from '../actions'
 
 class ChessboardContainer extends Component {
-  state = [
-    {
-      id: 1,
-      type: 'K',
-      color: 'black',
-      column: 1,
-      row: 0,
-      game_id: 1
+  state = {
+    board: Array(8).fill(Array(8).fill({
+      imgsrc: null,
+      type: null,
+      color: null,
+      coordinate_X: null,
+      coordinate_Y: null,
+      figureId: null
+    })),
+    hasSelected: false,
+    selectedPiece: {
+      type: null,
+      color: null,
+      coordinate_X: null,
+      coordinate_Y: null,
+      figureId: null
     }
-  ]
+  }
+  componentDidMount() {
+    const game = this.props.games.find(game => game.id === parseInt(this.props.gameID))
+    const newBoard = this.state.board.map((row, rowIndex) => row.map((square, squareIndex) => {
+      return {
+        imgsrc: null,
+        type: null,
+        color: null,
+        coordinate_X: squareIndex,
+        coordinate_Y: rowIndex,
+        figureId: null
+      }
+    }))
+    if (game) {
+      if (game.figures) {
+        game.figures.forEach(figure => {
+          newBoard[figure['coordinate_Y']][figure['coordinate_X']] = {
+            imgsrc: imgSrcAssigner(figure['kind'], figure['color'], figure['coordinate_X'], figure['coordinate_Y']),
+            type: figure['kind'],
+            color: figure['color'],
+            coordinate_X: figure['coordinate_X'],
+            coordinate_Y: figure['coordinate_Y'],
+            figureId: figure['id']
+          }
+        })
+      }
+    }
+    newBoard.reverse()
+    this.setState({
+      board: newBoard
+    })
+  }
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      const game = this.props.games.find(game => game.id === parseInt(this.props.gameID))
+      const newBoard = this.state.board.map((row, rowIndex) => row.map((square, squareIndex) => {
+        return {
+          imgsrc: null,
+          type: null,
+          color: null,
+          coordinate_X: squareIndex,
+          coordinate_Y: rowIndex,
+          figureId: null
+        }
+      }))
+      if (game.figures) {
+        game.figures.forEach(figure => {
+          newBoard[figure['coordinate_Y']][figure['coordinate_X']] = {
+            imgsrc: imgSrcAssigner(figure['kind'], figure['color'], figure['coordinate_X'], figure['coordinate_Y']),
+            type: figure['kind'],
+            color: figure['color'],
+            coordinate_X: figure['coordinate_X'],
+            coordinate_Y: figure['coordinate_Y'],
+            figureId: figure['id']
+          }
+        })
+      }
+      newBoard.reverse()
+      this.setState({
+        board: newBoard
+      })
+    }
+  }
+  onClick = (piece) => {
+    console.log('clicked!', piece)
+    console.log('hasbeenselected', this.state.hasSelected)
+    if (!this.state.hasSelected && piece['type'] !== null) {
+      this.setState({
+        hasSelected: true,
+        selectedPiece: {
+          type: piece['type'],
+          color: piece['color'],
+          coordinate_X: piece['coordinate_X'],
+          coordinate_Y: piece['coordinate_Y'],
+          figureId: piece['figureId']
+        }
+      })
+    } else if (this.state.hasSelected) {
+      const newBoard = this.state.board.map(row => [...row])
+      newBoard[this.state.selectedPiece.coordinate_Y][this.state.selectedPiece.coordinate_X]['imgsrc'] = null
+      this.setState({
+        hasSelected: false,
+        board: newBoard
+      })
+      this.props.dispatch(makeMove(this.state.selectedPiece.figureId, piece.coordinate_X, piece.coordinate_Y, parseInt(this.props.gameID)))
+    }
+  }
   render() {
     return (
       <div>
-        <Chessboard />
+        <Chessboard board={this.state.board} onClick={this.onClick} />
       </div>
     );
   }
 }
 
-export default ChessboardContainer;
+const mapStateToProps = (state) => ({
+  games: state.listOfGames,
+  user: state.currentUser
+})
+
+export default connect(mapStateToProps)(ChessboardContainer);
