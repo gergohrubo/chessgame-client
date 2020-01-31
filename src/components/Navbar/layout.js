5a07dce4dc4116c0da03a4787711c3bb4c0a10a6
gergohrubo <gergohrubo@gmail.com>
2020-01-31 14:32:38 +0100

    Logout link fix

--- src/components/Navbar/layout.js
+++ src/components/Navbar/layout.js
@@ -8,7 +8,7 @@ function Navbar(props) {
     <div className={classes.navbar}>
       <Link to="/"> Homepage</Link>
       {!props.user.username && <Link to="/login"> Login</Link>}
-      {props.user.username && <div className={classes.logout} onClick={props.onClick}><Link to='/'>Logout</Link></div>}
+      {props.user.username && <div className={classes.logout} onClick={props.onClick}><Link to='/'> Logout</Link></div>}
       {props.user.username && <p className={classes.navbarUsername}>{props.user.username}</p>}
     </div>
   );
